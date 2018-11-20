90cbd0f42a137718460bdebd77a7c6c307d7a35b
stella <3291570994@qq.com>
2018-11-20 18:01:41 +0800

    es6 number

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 // import './source/proxy';
 // import './source/arrow-function';
 // import './source/parameter'
-import './source/string'
+//import './source/string'
+import './source/number'
 
 let log=(item)=>{console.log(item)};
 log('我要学习ES6');
---
+++ source/number.js
@@ -0,0 +1,19 @@
+/**
+ * 二进制和八进制表示法
+ * ES6提供了二进制和八进制数值的新的写法，分别用前缀0b（或0B）和0o（或0O）表示。
+ * 如果要将0b和0o前缀的字符串数值转为十进制，要使用Number方法。
+ */
+{
+    console.log(0b111110111 === 503);//true 二进制 -- 十进制
+    console.log(0o767 === 503);// true 八进制 -- 十进制
+
+    console.log(Number('0b111'));//7 二进制转成十进制
+    console.log(Number('0o10'));//7 八进制转成十进制
+}
+
+/**
+ * Number.isFinite(), Number.isNaN();
+ * ES6在Number对象上，新提供了Number.isFinite()和Number.isNaN()两个方法。
+ * Number.isFinite()用来检查一个数值是否为有限的（finite）。
+ * Number.isNaN()用来检查一个值是否为NaN。
+ */
